8d0f6c6adeae82b021af675eb5847a30fabae20f
unknown <193280@ids.upchiapas.edu.mx>
2022-03-09 22:49:30 -0600

    avance registro

--- duenios/src/main/java/floresnataren/duenios/repositorio/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package floresnataren.duenios.repositorio;
-
-import floresnataren.duenios.modelo.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Usuario,Integer> {
-    Usuario save(Usuario user);
-}
