6e9f4621bbcdefd6ecfbdf3a80745954eaa649c0
Javier Gutierrez <gugodin@hotmail.com>
2022-03-14 09:13:05 -0600

    agregando estilo

--- vista-veterinaria/src/Componentes/VistaDuenios/VistaDuenio.js
+++ vista-veterinaria/src/Componentes/VistaDuenios/VistaDuenio.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import axios from 'axios';
+import '../assets/css/registro.css';
 import { useState } from 'react';
-import registroDuenioM from './VistaDuenioM'
+import registroDuenioM from './VistaDuenioM';
+import ProductoImagen from '../ProductoImagen';
+import img from '../assets/Imagenes/mascotas_logo.png';
+
 
 
 export default function Register(){
@@ -26,42 +30,50 @@ export default function Register(){
     }
     
     return(
-        <div>
+        <div className='total'>
             <div className='registroDuenio'>
                 <h2 className='titulo'>Registro de nuevo dueño</h2>
                 <br />
-                <div className="inputs" class="input-group mb-3">
-                    <ul>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="nombre" onChange={e => setPrimerNombre(e.target.value)}>Nombre del dueño</span>
-                        <input type="text" class="form-control"  aria-describedby="basic-addon1"/>
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="apellido" onChange={e => setApellido(e.target.value)}>Apellido del dueño</span>
-                        <input type="text" class="form-control"  aria-describedby="basic-addon1"/>
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="nombre" onChange={e => setNombre(e.target.value)}>Usuario</span>
-                        <input type="text" class="form-control"  aria-describedby="basic-addon1"/>
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="password" onChange={e => setContrasenia(e.target.value)}>Contraseña</span>
-                        <input type="password" class="form-control"  aria-describedby="basic-addon1"/>
-                    </div>
-                    <select class="form-select" aria-label="Default select example" onChange={e => setRol(e.target.value)}>
-                        <option selected>Tipo de usuario</option>
-                        <option value="DUEÑO">Cliente</option>
-                        <option value="EMPLEADO">Trabajador</option>
-        
-                    </select>
-                   
+                <div className='FormularioRegistro'>
+                    <div className="inputs" class="input-group mb-3">
+                        <ul>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="nombre" onChange={e => setPrimerNombre(e.target.value)}>Nombre del dueño</span>
+                            <input type="text" class="form-control"  aria-describedby="basic-addon1"/>
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="apellido" onChange={e => setApellido(e.target.value)}>Apellido del dueño</span>
+                            <input type="text" class="form-control"  aria-describedby="basic-addon1"/>
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="nombre" onChange={e => setNombre(e.target.value)}>Usuario</span>
+                            <input type="text" class="form-control"  aria-describedby="basic-addon1"/>
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="password" onChange={e => setContrasenia(e.target.value)}>Contraseña</span>
+                            <input type="password" class="form-control"  aria-describedby="basic-addon1"/>
+                        </div>
+                        <select class="form-select" aria-label="Default select example" onChange={e => setRol(e.target.value)}>
+                            <option selected>Tipo de usuario</option>
+                            <option value="DUEÑO">Cliente</option>
+                            <option value="EMPLEADO">Trabajador</option>
+            
+                        </select>
                     
-                    <button id= 'boton' type="button" class="btn btn-outline-success" onClick={() => {handleRegister()}}>Registrar dueño</button>
-                    </ul>
+                        <br></br>
+                        <button id= 'boton' type="button" class="btn btn-outline-success" onClick={() => {handleRegister()}}>Registrar dueño</button>
+                        </ul>
 
+                    </div>
                 </div>
             </div>
-        {/* {<registroDuenioM/>} */}
+            <div className='contenedorInfo'>
+                <div className="logo">
+                    <ProductoImagen url={img} clase='imgLogo'/>
+                </div>
+            </div>
+                
+            
         </div>
 
 
